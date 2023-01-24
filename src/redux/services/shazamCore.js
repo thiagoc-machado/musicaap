d8e0d5462a544f62793e3448d91edfd8f52caa4d
thiagoc-machado <thiagocmachado@yahoo.com.br>
2023-01-25 00:32:16 +0100

    rapidApi

---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,18 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "14feb92d3emsh5bf7deab2e644f8p1c8a21jsn96f8e9290a0e",
+    "X-RapidAPI-Host": "shazam-core.p.rapidapi.com",
+  },
+};
+
+fetch("https://shazam-core.p.rapidapi.com/v1/charts/world", options)
+  .then((response) => response.json())
+  .then((response) => console.log(response))
+  .catch((err) => console.error(err));
+
+export const shazamCoreApi = createApi({
+  reducerPath: 'shazamCoreApi',
+});
