95d5791875bacc328674bd2d30f2207684446861
thiagoc-machado <thiagocmachado@yahoo.com.br>
2023-01-25 01:17:27 +0100

    loader

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,9 +1,12 @@
-import { Error, Loader, SongCard } from "../components";
-import { genres } from "../assets/constants";
+import { Error, Loader, SongCard } from '../components';
+import { genres } from '../assets/constants';
+import { useGetTopChartsQuery } from '../redux/services/shazamCore';
 
 const Discover = () => {
-  const genreTitle = "Pop";
-
+  const { data, isFetching, error } = useGetTopChartsQuery();
+  const genreTitle = 'Pop';
+  if (isFetching) return <Loader title="Loading songs..." />;
+  if (error) return <Error />;
   return (
     <div className="flex flex-col">
       <div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10">
@@ -12,18 +15,18 @@ const Discover = () => {
         </h2>
         <select
           onChange={() => {}}
-          value=""
+          value=''
           className="bg-black text-gray-300 p-3 text-sm rounded-lg outline-none sm:mt-0 mt-5"
         >
           {genres.map((genre) => (
             <option key={genre.value} value={genre.value}>
-              {genre.title}{" "}
+              {genre.title}{' '}
             </option>
           ))}
         </select>
       </div>
       <div className="flex flex-wrap sm:justify-start justify-center gap-8">
-        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((song, i) => (
+        {data?.map((song, i) => (
           <SongCard key={song.key} song={song} i={i} />
         ))}
       </div>
--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -1,18 +1,23 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-
-const options = {
-  method: "GET",
-  headers: {
-    "X-RapidAPI-Key": "14feb92d3emsh5bf7deab2e644f8p1c8a21jsn96f8e9290a0e",
-    "X-RapidAPI-Host": "shazam-core.p.rapidapi.com",
-  },
-};
-
-fetch("https://shazam-core.p.rapidapi.com/v1/charts/world", options)
-  .then((response) => response.json())
-  .then((response) => console.log(response))
-  .catch((err) => console.error(err));
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const shazamCoreApi = createApi({
   reducerPath: 'shazamCoreApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
+    prepareHeaders: (headers) => {
+      headers.set(
+        'X-RapidAPI-Key',
+        '14feb92d3emsh5bf7deab2e644f8p1c8a21jsn96f8e9290a0e',
+      );
+
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({ query: () => '/charts/world' }),
+  }),
 });
+
+export const{
+  useGetTopChartsQuery,
+} = shazamCoreApi;
